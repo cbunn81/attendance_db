f07a4e6de0a79a60324563dcaf21fb701e97db83
Christopher Bunn <github@thenerdery.org>
2017-07-27 14:17:53 +0900

    Fixed enter_data so that non-graded classes don't have the grade form elements.

--- enter_data.php
+++ enter_data.php
@@ -9,6 +9,7 @@ $dow = date("l", strtotime($date));
 $class_id = $_SESSION["class_id"] = $_GET["cid"] ?? $_SESSION["class_id"] ?? NULL;
 
 require_once('../../config/db.inc.php');
+require_once('includes/common.inc.php');
 ?>
 
 <!DOCTYPE html>
@@ -34,11 +35,15 @@ echo "<p>Day of the week: $dow</p>";
 			<td>Student ID</td>
 			<td>Student Name</td>
 			<td>Present?</td>
-			<td>Speaking</td>
+<?php
+	if(is_graded_class($class_id)) {
+		echo "<td>Speaking</td>
 			<td>Listening</td>
 			<td>Reading</td>
 			<td>Writing</td>
-			<td>Behavior</td>
+			<td>Behavior</td>";
+	}
+?>
 			<td>Notes</td>
 		</tr>
 	</thead>
@@ -60,8 +65,9 @@ if ($stmt->rowCount()) {
 		echo "<tr><td>" . htmlspecialchars($row['student_id'], ENT_QUOTES, 'UTF-8') . "</td>
 				<td>" . htmlspecialchars($row['student_name'], ENT_QUOTES, 'UTF-8') . "</td>
 				<td><input type=\"hidden\" name=\"adata[" . htmlspecialchars($row['student_id'], ENT_QUOTES, 'UTF-8') . "][present]\" value=\"0\" />
-				    <input type=\"checkbox\" id=\"present\" name=\"adata[" . htmlspecialchars($row['student_id'], ENT_QUOTES, 'UTF-8') . "][present]\" value=\"1\" /></td>
-				<td><select id=\"speaking\" name=\"adata[" . htmlspecialchars($row['student_id'], ENT_QUOTES, 'UTF-8') . "][speaking]\">
+				    <input type=\"checkbox\" id=\"present\" name=\"adata[" . htmlspecialchars($row['student_id'], ENT_QUOTES, 'UTF-8') . "][present]\" value=\"1\" /></td>";
+		if(is_graded_class($class_id)) {
+			echo "<td><select id=\"speaking\" name=\"adata[" . htmlspecialchars($row['student_id'], ENT_QUOTES, 'UTF-8') . "][speaking]\">
 						<option selected value=\"0\">0</option>
 						<option value=\"1\">1</option>
 						<option value=\"2\">2</option>
@@ -100,8 +106,9 @@ if ($stmt->rowCount()) {
 						<option value=\"3\">3</option>
 						<option value=\"4\">4</option>
 						<option value=\"5\">5</option>
-					</select></td>
-				<td><textarea name=\"adata[" . htmlspecialchars($row['student_id'], ENT_QUOTES, 'UTF-8') . "][notes]\" cols=\"30\" rows=\"2\"></textarea></td>";
+					</select></td>";
+		}
+				echo "<td><textarea name=\"adata[" . htmlspecialchars($row['student_id'], ENT_QUOTES, 'UTF-8') . "][notes]\" cols=\"30\" rows=\"2\"></textarea></td>";
 	}
 }
 else {
