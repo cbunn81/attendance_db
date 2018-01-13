1c0ff6f78b304144710c6e29076235cbe6285d3d
Christopher Bunn <github@thenerdery.org>
2018-01-13 13:06:29 +0900

    Changed grade report to print more than one class if necessary.

--- print_grades.php
+++ print_grades.php
@@ -65,11 +65,11 @@ if(is_graded_class($class_id)) {
 	$grades_count = array_fill(1,5,0);
 
 	// Create query to get all attendance ids for the student
-// XXXX - ONLY FOR TEST 2 PERIOD!! - XXXX
+// XXXX - ONLY FOR TEST 3 PERIOD!! - XXXX
 	$attendance_stmt = $pdo->prepare("SELECT a.attendance_id, ci.cinstance_id, ci.cinstance_date, a.present, a.notes
 	  FROM attendance a
 	  INNER JOIN class_instances ci ON a.cinstance_id = ci.cinstance_id
-	  WHERE a.student_id = :student_id AND ci.cinstance_date BETWEEN '2017-06-26' AND '2017-09-16'
+	  WHERE a.student_id = :student_id AND ci.cinstance_date BETWEEN '2017-09-17' AND '2017-12-16'
 		ORDER BY ci.cinstance_date");
 	$attendance_stmt->execute(['student_id' => $student_id]);
 
@@ -196,7 +196,7 @@ if(is_graded_class($class_id)) {
 		"total" => 19.0,
 	);
 ?>
-	<h2>Test #2 Results (<?php echo $level_name; ?>)</h2>
+	<h2>Test #3 Results (<?php echo $level_name; ?>)</h2>
 
 	<table>
 		<thead>
