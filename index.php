bc789ff30dad66c93034eb6050762a8dca566cf1
Christopher Bunn <github@thenerdery.org>
2018-06-26 02:27:16 +0900

    Add test option to main page.

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ require_once('../../config/db.inc.php');
 
 <li><a href="choose_teacher.php">Enter Attendance Data</a></li>
 <li><a href="choose_location.php?is_makeup=true">Enter Makeup Information</a></li>
+<li><a href="choose_teacher.php?is_test=true">Enter Test Results</a></li>
 <li><a href="list_students.php">Print an Attendance Report</a></li>
 
 </ul>
