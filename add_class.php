7c360c461b0537203e36f146c5d1628bb593535a
Christopher Bunn <github@thenerdery.org>
2019-04-23 15:06:43 +0900

    Finish markup and add locations to script.

--- add_class.php
+++ add_class.php
@@ -21,16 +21,21 @@ echo "<p>Day of the week: $dow</p>";
 ?>
 
 <h1>Create a New Class:</h1>
-<form action="submit_newclass.php" method="post">
+<form id="addclass" action="submit_newclass.php" method="post">
 	<fieldset>
 		<legend>Class Details</legend>
 		<ol>
 			<li>
 				<label for="location">Location:</label>
 				<select id="location" name="location">
-		        <option value="Anan">Anan</option>
-		        <option value="Happy">Happy</option>
-		        <option value="Minami-showa-ko">Minami-showa-ko</option>
+<?php
+/* List locations */
+$locations = get_locations();
+foreach ($locations as $location)
+{
+	echo "<option value=\"" . $location['location_id'] . "\">" . $location['location_name'] . "</option>";
+}
+?>
 		    </select>
 			</li>
 			<li>
@@ -68,22 +73,49 @@ echo "<p>Day of the week: $dow</p>";
 				<input id="class_time" name="class_time" type="time" step="300" required />
 			</li>
 			<li>
-				<label for="teacher">Teacher:</label>
-				<select id="teacher" name="teacher">
-		        <option value="Ahleen">Ahleen</option>
-		        <option value="Chris">Chris</option>
-		        <option value="Jill">Jill</option>
-						<option value="Shayne">Shayne</option>
-		    </select>
-			</li>
-			<li>
 				<label for="start_date">Start Date:</label>
 				<input id="start_date" name="start_date" type="date" value="2019-04-01" required />
 			</li>
 			<li>
 				<label for="end_date">End Date:</label>
 				<input id="end_date" name="end_date" type="date" />
-				<span> (leave blank if no end date yet)</span>
+				<span class="hint"> (Leave blank if no end date yet)</span>
+			</li>
+		</ol>
+	</fieldset>
+	<fieldset>
+		<legend>Roster Details</legend>
+		<ol>
+			<li>
+				<label for="teacher">Teacher:</label>
+				<select id="teacher" name="teacher">
+						<option value="Ahleen">Ahleen</option>
+						<option value="Chris">Chris</option>
+						<option value="Jill">Jill</option>
+						<option value="Shayne">Shayne</option>
+				</select>
+			</li>
+			<li>
+				<label for="students">Students:</label>
+				<select multiple size=10 id="teacher" name="teacher">
+						<option value="1">First Student</option>
+						<option value="2">Second Student</option>
+						<option value="3">Third Student</option>
+						<option value="4">Fourth Student</option>
+						<option value="5">Fifth Student</option>
+						<option value="6">Sixth Student</option>
+						<option value="7">Seventh Student</option>
+						<option value="8">Eighth Student</option>
+						<option value="9">Ninth Student</option>
+						<option value="10">Tenth Student</option>
+						<option value="11">Eleventh Student</option>
+						<option value="12">Twelfth Student</option>
+						<option value="13">Thirteenth Student</option>
+						<option value="14">Fourteenth Student</option>
+						<option value="15">Fifteenth Student</option>
+						<option value="16">Sixteenth Student</option>
+				</select>
+				<span class="hint"> (Ctrl-click or Cmd-click to select multiple students)</span>
 			</li>
 		</ol>
 		<input type="submit" />
