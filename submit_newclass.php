2416ff47752c6f83bdea17b1e111bcac960b070b
Christopher Bunn <github@thenerdery.org>
2019-04-26 22:11:35 +0900

    Added helpful text to the confirmaiton page.

--- submit_newclass.php
+++ submit_newclass.php
@@ -14,12 +14,6 @@ require_once('includes/model.php');
 
 <?php
 
-/*
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-*/
-
 // The information has not been confirmed yet
 if(empty($_SESSION["confirm"])) {
 	// Set a session variable to skip this part on confirmation
@@ -28,43 +22,34 @@ if(empty($_SESSION["confirm"])) {
 	// Sanitize POST variables
 	$_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
+	// Set local and session variables from the form data
 	$location_id = $_SESSION["location_id"] = $_POST['location'];
 	$dow_id = $_SESSION["dow_id"] = $_POST['dow'];
 	$ctype_id = $_SESSION["ctype_id"] = $_POST['ctype'];
 	$level_id = $_SESSION["level_id"] = $_POST['level'];
 	$class_time = $_SESSION["class_time"] = $_POST['class_time'];
 	$start_date = $_SESSION["start_date"] = $_POST['start_date'];
-	$end_date = $_SESSION["end_date"] = $_POST['end_date'] ?: "infinity";
+	$end_date = $_SESSION["end_date"] = $_POST['end_date'] ?: "infinity"; // "infinity" means no end date
 	$teacher_id = $_SESSION["teacher_id"] = $_POST['teacher'];
-	$student_ids = $_SESSION["student_ids"] = $_POST['students'];
+	$student_ids = $_SESSION["student_ids"] = $_POST['students']; // should be an array of integers
 
+	// get names instead of ID numbers for user confirmation
 	$location_name = get_location_by_id($location_id);
 	$dow_name = get_dow_by_id($dow_id);
 	$ctype_name = get_ctype_by_id($ctype_id);
 	$level_name = get_level_by_id($level_id);
 	$teacher_name = get_person_name($teacher_id);
+	// Since students are in an array, we'll get them as we output
 
-	echo "<h1>Please confirm the information submitted for the new class</h1>";
-	//echo "<p>Location ID: ". htmlspecialchars($location_id, ENT_QUOTES, 'UTF-8') ."</p>";
+	echo "<h1>Please confirm the following information submitted for the new class</h1>";
 	echo "<p>Location Name: ". htmlspecialchars($location_name, ENT_QUOTES, 'UTF-8') ."</p>";
-	//echo "<p>DOW ID: ". htmlspecialchars($dow_id, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>DOW: ". htmlspecialchars($dow_name, ENT_QUOTES, 'UTF-8') ."</p>";
-	//echo "<p>Class Type ID: ". htmlspecialchars($ctype_id, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Class Type: ". htmlspecialchars($ctype_name, ENT_QUOTES, 'UTF-8') ."</p>";
-	//echo "<p>Level ID: ". htmlspecialchars($level_id, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Level: ". htmlspecialchars($level_name, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Class Time: ". htmlspecialchars($class_time, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Start Date: ". htmlspecialchars($start_date, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>End Date: ". htmlspecialchars($end_date, ENT_QUOTES, 'UTF-8') ."</p>";
-	//echo "<p>Teacher ID: ". htmlspecialchars($teacher_id, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Teacher Name: ". htmlspecialchars($teacher_name, ENT_QUOTES, 'UTF-8') ."</p>";
-	/*
-	echo "<p>Student IDs: ";
-	foreach($student_ids as $student_id) {
-		echo htmlspecialchars($student_id, ENT_QUOTES, 'UTF-8') .", ";
-	}
-	echo "</p>";
-	*/
 	echo "<p>Student Names: ";
 	echo "<ul>";
 	foreach($student_ids as $student_id) {
@@ -91,36 +76,22 @@ else {
 	$end_date = $_SESSION["end_date"];
 	$teacher_id = $_SESSION["teacher_id"];
 	$student_ids = $_SESSION["student_ids"];
-
+/*
 	$location_name = get_location_by_id($location_id);
 	$dow_name = get_dow_by_id($dow_id);
 	$ctype_name = get_ctype_by_id($ctype_id);
 	$level_name = get_level_by_id($level_id);
 	$teacher_name = get_person_name($teacher_id);
 
-	echo "<h1>Information Confirmed</h1>";
-
 	echo "<h1>Please confirm the information submitted for the new class</h1>";
-	//echo "<p>Location ID: ". htmlspecialchars($location_id, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Location Name: ". htmlspecialchars($location_name, ENT_QUOTES, 'UTF-8') ."</p>";
-	//echo "<p>DOW ID: ". htmlspecialchars($dow_id, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>DOW: ". htmlspecialchars($dow_name, ENT_QUOTES, 'UTF-8') ."</p>";
-	//echo "<p>Class Type ID: ". htmlspecialchars($ctype_id, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Class Type: ". htmlspecialchars($ctype_name, ENT_QUOTES, 'UTF-8') ."</p>";
-	//echo "<p>Level ID: ". htmlspecialchars($level_id, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Level: ". htmlspecialchars($level_name, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Class Time: ". htmlspecialchars($class_time, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Start Date: ". htmlspecialchars($start_date, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>End Date: ". htmlspecialchars($end_date, ENT_QUOTES, 'UTF-8') ."</p>";
-	//echo "<p>Teacher ID: ". htmlspecialchars($teacher_id, ENT_QUOTES, 'UTF-8') ."</p>";
 	echo "<p>Teacher Name: ". htmlspecialchars($teacher_name, ENT_QUOTES, 'UTF-8') ."</p>";
-	/*
-	echo "<p>Student IDs: ";
-	foreach($student_ids as $student_id) {
-		echo htmlspecialchars($student_id, ENT_QUOTES, 'UTF-8') .", ";
-	}
-	echo "</p>";
-	*/
 	echo "<p>Student Names: ";
 	echo "<ul>";
 	foreach($student_ids as $student_id) {
@@ -129,22 +100,34 @@ else {
 	}
 	echo "</ul>";
 	echo "</p>";
-/*
-	echo "<p>Attempt class insert ....</p>";
-	$class_id = create_new_class($location_id,$dow_id,$ctype_id,$level_id,$class_time,$start_date,$end_date);
+*/
+	echo "<h1>Information Confirmed</h1>";
+	echo "<p>Attempting to insert the new class ...</p>";
+	if($class_id = create_new_class($location_id,$dow_id,$ctype_id,$level_id,$class_time,$start_date,$end_date)) {
+		echo "<p>New class inserted successfully!</p>";
+		echo "<p>New Class ID: $class_id</p>";
+	}
+	else {
+		echo "<p>An error has occurred while inserting the new class.</p>";
+	}
 
-	echo "<p>New Class ID: $class_id</p>";
-
-	echo "<p>Attempt Roster insert ...</p>";
+	echo "<p>Attempting to insert the new entries in the roster ...</p>";
 	if (create_roster_entry($teacher_id,$class_id,$start_date,$end_date)) {
-		echo "<p>Success on inserting teacher ID $teacher_id!</p>";
+		$teacher_name = get_person_name($teacher_id);
+		echo "<p>\"". htmlspecialchars($teacher_name, ENT_QUOTES, 'UTF-8') ."\" successfully inserted into the roster as teacher.</p>";
+	}
+	else {
+		echo "<p>An error has occurred while inserting the teacher into the roster.</p>";
 	}
 	foreach($student_ids as $student_id) {
 		if (create_roster_entry($student_id,$class_id,$start_date,$end_date)) {
-			echo "<p>Success on inserting student ID: $student_id!</p>";
+			$student_name = get_person_name($student_id);
+			echo "<p>\"". htmlspecialchars($student_name, ENT_QUOTES, 'UTF-8') ."\" successfully inserted into the roster as a student.</p>";
+		}
+		else {
+			echo "<p>An error has occurred while inserting a student into the roster.</p>";
 		}
 	}
-*/
 }
 ?>
 
