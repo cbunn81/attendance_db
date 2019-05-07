aade65b0acccfe1a36e3af753bcd1a33bf51152b
Christopher Bunn <github@thenerdery.org>
2019-05-07 13:42:32 +0900

    Getting person's data from ID works.

--- edit_person.php
+++ edit_person.php
@@ -2,7 +2,7 @@
 // Start the session
 session_start();
 // Get the Student ID from the URL or from a Session Variable
-$student_id = $_SESSION["student_id"] = $_GET["sid"] ?? $_SESSION["student_id"] ?? NULL;
+$person_id = $_SESSION["person_id"] = $_GET["pid"] ?? $_SESSION["person_id"] ?? NULL;
 
 require_once('includes/model.php');
 ?>
@@ -10,76 +10,25 @@ require_once('includes/model.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Add a New Person</title>
+	<title>Edit a Person</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="css/main.css">
 </head>
 <body>
 
-<h1>Add a New Person to the System:</h1>
-<form id="addperson" class="addinfo" action="submit_newperson.php" method="post">
-	<fieldset>
-		<legend>Basic Details</legend>
-		<ol>
-			<li>
-				<label for="person_type">Person Type:</label>
-				<!-- Staff vs. Student -->
-				<select id="person_type" name="person_type">
 <?php
-/* List genders */
-$person_types = get_person_types();
-foreach ($person_types as $person_type)
-{
-	echo "<option value=\"" . htmlspecialchars($person_type['ptype_id'], ENT_QUOTES, 'UTF-8') . "\">" . htmlspecialchars($person_type['ptype_name'], ENT_QUOTES, 'UTF-8') . "</option>\n";
+
+if(isset($person_id)) {
+	$person_info = get_person_info($person_id);
+	echo "<pre>";
+	print_r($person_info);
+	echo "</pre>";
+	echo "<h1>Edit " . htmlspecialchars($person_info['given_name_r'], ENT_QUOTES, 'UTF-8') . " " . htmlspecialchars($person_info['family_name_r'], ENT_QUOTES, 'UTF-8') . "'s Data:</h1>";
 }
-?>
-				</select>
-			</li>
-			<li>
-				<label for="family_name_k">Family Name <span class="hint">(in Kanji/Kana)</span>:</label>
-				<input type="text" id="family_name_k" name="family_name_k" size="30" required />
-			</li>
-			<li>
-				<label for="given_name_k">Given Name <span class="hint">(in Kanji/Kana)</span>:</label>
-				<input type="text" id="given_name_k" name="given_name_k" size="30" required />
-			</li>
-			<li>
-				<label for="family_name_k">Family Name <span class="hint">(in Romaji)</span>:</label>
-				<input type="text" id="family_name_r" name="family_name_r" size="30" required />
-			</li>
-			<li>
-				<label for="given_name_k">Given Name <span class="hint">(in Romaji)</span>:</label>
-				<input type="text" id="given_name_r" name="given_name_r" size="30" required />
-			</li>
-			<li>
-				<label for="dob">Date of Birth:</label>
-				<input id="dob" name="dob" type="date" />
-				<span class="hint"> (Optional)</span>
-			</li>
-			<li>
-				<label for="gender">Gender:</label>
-				<select id="gender" name="gender">
-<?php
-/* List genders */
-$genders = get_genders();
-foreach ($genders as $gender)
-{
-	echo "<option value=\"" . htmlspecialchars($gender['gender_id'], ENT_QUOTES, 'UTF-8') . "\">" . htmlspecialchars($gender['gender_name'], ENT_QUOTES, 'UTF-8') . "</option>\n";
+else {
+	echo "No person has been selected, please try again.";
 }
+
 ?>
-		    </select>
-			</li>
-			<li>
-				<label for="start_date">Start Date:</label>
-				<input id="start_date" name="start_date" type="date" value="2019-04-01" required />
-			</li>
-			<li>
-				<label for="end_date">End Date:</label>
-				<input id="end_date" name="end_date" type="date" />
-				<span class="hint"> (Leave blank if no end date yet)</span>
-			</li>
-		</ol>
-	</fieldset>
-	<input type="submit" />
-</form>
+
 </body>
 </html>
