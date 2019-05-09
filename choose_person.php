fad257cdb247ce15dbc031099d9d0e7ed9753a24
Christopher Bunn <github@thenerdery.org>
2019-05-09 21:14:54 +0900

    Change name of file (and links to it) to better reflect it is for both adding and editing person data.

--- choose_person.php
+++ choose_person.php
@@ -19,7 +19,7 @@ require_once('includes/model.php');
 $teachers = get_all_teachers();
 foreach ($teachers as $teacher)
 {
-    echo "<li><a href=\"edit_person.php?pid=" . htmlspecialchars($teacher['person_id'], ENT_QUOTES, 'UTF-8') . "\">" .
+    echo "<li><a href=\"enter_person_data.php?pid=" . htmlspecialchars($teacher['person_id'], ENT_QUOTES, 'UTF-8') . "\">" .
     htmlspecialchars($teacher['given_name_r'], ENT_QUOTES, 'UTF-8') . " " . htmlspecialchars($teacher['family_name_r'], ENT_QUOTES, 'UTF-8') . "</a></li>\r\n";
 }
 ?>
@@ -33,7 +33,7 @@ foreach ($teachers as $teacher)
 $students = get_all_students();
 foreach ($students as $student)
 {
-    echo "<li><a href=\"edit_person.php?pid=" . htmlspecialchars($student['person_id'], ENT_QUOTES, 'UTF-8') . "\">" .
+    echo "<li><a href=\"enter_person_data.php?pid=" . htmlspecialchars($student['person_id'], ENT_QUOTES, 'UTF-8') . "\">" .
 		htmlspecialchars($student['family_name_r'], ENT_QUOTES, 'UTF-8') . ", " . htmlspecialchars($student['given_name_r'], ENT_QUOTES, 'UTF-8') . " (" .
 		htmlspecialchars($student['family_name_k'], ENT_QUOTES, 'UTF-8') . " " . htmlspecialchars($student['given_name_k'], ENT_QUOTES, 'UTF-8') . ")</a></li>\r\n";
 }
