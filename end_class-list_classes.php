dd4510df304aba5c4e0ab8170eee4208d38ed5cd
Christopher Bunn <github@thenerdery.org>
2019-05-15 22:03:01 +0900

    Add list of students for each class as a hint and block showing of Flex classes under other days of the week.

--- end_class-list_classes.php
+++ end_class-list_classes.php
@@ -29,21 +29,24 @@ foreach ($days_of_week as $day_of_week) {
 	if($classes = get_classes_for_teacher($teacher_id,$day_of_week['dow_name'],$date)) {
 		echo "<ol>\r\n";
 		foreach ($classes as $class) {
-			$students = get_students_for_class($class['class_id'], $teacher_id, $date);
-			echo "<li><input type=\"checkbox\" id=\"class-" . htmlspecialchars($class['class_id'], ENT_QUOTES, 'UTF-8') . "\"
-																		 name=\"" . htmlspecialchars($class['class_id'], ENT_QUOTES, 'UTF-8') . "\" />";
-			echo "<label for==\"class-" . htmlspecialchars($class['class_id'], ENT_QUOTES, 'UTF-8') . "\">" .
-									 htmlspecialchars($class['class_id'], ENT_QUOTES, 'UTF-8') . " - " .
-									 htmlspecialchars($class['class_time'], ENT_QUOTES, 'UTF-8') . " - " .
-									 htmlspecialchars($class['level_name'], ENT_QUOTES, 'UTF-8') . " - " .
-									 "Start Date (" . htmlspecialchars($class['start_date'], ENT_QUOTES, 'UTF-8') . ")</label>";
-			echo "<span class=\"hint\">(Students: ";
- 			foreach ($students as $student) {
- 				echo htmlspecialchars($student['student_name'], ENT_QUOTES, 'UTF-8') . ",";
- 			}
- 			echo ")</span>\r\n";
-			echo "</li>\r\n";
-
+			if (($class['dow_name'] != "Flex") || ($day_of_week['dow_name'] == "Flex")) {
+				$students = get_students_for_class($class['class_id'], $teacher_id, $date);
+				echo "<li><input type=\"checkbox\" id=\"class-" . htmlspecialchars($class['class_id'], ENT_QUOTES, 'UTF-8') . "\"
+																			 name=\"" . htmlspecialchars($class['class_id'], ENT_QUOTES, 'UTF-8') . "\" />";
+				echo "<label for==\"class-" . htmlspecialchars($class['class_id'], ENT_QUOTES, 'UTF-8') . "\">" .
+										 htmlspecialchars($class['class_id'], ENT_QUOTES, 'UTF-8') . " - " .
+										 htmlspecialchars($class['class_time'], ENT_QUOTES, 'UTF-8') . " - " .
+										 htmlspecialchars($class['level_name'], ENT_QUOTES, 'UTF-8') . " - " .
+										 "Start Date (" . htmlspecialchars($class['start_date'], ENT_QUOTES, 'UTF-8') . ")</label>";
+				echo "<span class=\"hint\">(Students: ";
+				$student_names = array();
+	 			foreach ($students as $student) {
+	 				$student_names[] = $student['student_name'];
+	 			}
+				echo htmlspecialchars(implode(", ", $student_names), ENT_QUOTES, 'UTF-8');
+	 			echo ")</span>\r\n";
+				echo "</li>\r\n";
+			}
 		}
 		echo "</ol>\r\n";
 	}
