1353884f793ab0c3ee7287396374e56268f1aad9
Christopher Bunn <github@thenerdery.org>
2019-05-16 17:16:43 +0900

    Add display of confirmation info.

--- end_classes-submit.php
+++ end_classes-submit.php
@@ -2,7 +2,11 @@
 // Start the session
 session_start();
 
+$teacher_id = $_SESSION["teacher_id"] ?? NULL;
+$date = date("Y-m-d");
+
 require_once('includes/model.php');
+$teacher_name = get_person_name($teacher_id);
 ?>
 
 <!DOCTYPE html>
@@ -14,22 +18,40 @@ require_once('includes/model.php');
 <body>
 
 <?php
-$_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-/*
+
+
 // The information has not been confirmed yet
 if(empty($_POST["confirm"])) {
-	// Set a session variable to skip this part on confirmation
-	$_SESSION["confirm"] = TRUE;
-
-	// Sanitize POST variables
 	$_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
+	echo "<pre>";
+	print_r($_POST);
+	echo "</pre>";
+	echo "<h1>Please confirm that the following classes are correct</h1>";
+	echo "<table>\r\n<thead><tr><th>Class ID</th><th>Location</th><th>Day of the Week</th><th>Time</th><th>Level</th><th>Start Date</th><th>Teacher</th><th>Students</th></tr></thead>\r\n<tbody>\r\n";
+	// make an array of class IDs to use for the DB update later
+	$class_ids_to_end = array();
+	foreach($_POST as $class_id => $ending) {
+		$class_ids_to_end[] = $class_id;
+		$class_info = get_class_info($class_id);
+		$students = get_students_for_class($class_id, $teacher_id, $date);
+		$student_names = array();
+		foreach ($students as $student) {
+			$student_names[] = $student['student_name'];
+		}
+		$student_list = implode(", ", $student_names);
+		echo "<tr><td>" . htmlspecialchars($class_id, ENT_QUOTES, 'UTF-8') . "</td>\r\n";
+		echo "<td>" . htmlspecialchars($class_info['location_name'], ENT_QUOTES, 'UTF-8') . "</td>\r\n";
+		echo "<td>" . htmlspecialchars($class_info['dow_name'], ENT_QUOTES, 'UTF-8') . "</td>\r\n";
+		echo "<td>" . htmlspecialchars(substr($class_info['class_time'],0,-3), ENT_QUOTES, 'UTF-8') . "</td>\r\n";
+		echo "<td>" . htmlspecialchars($class_info['level_name'], ENT_QUOTES, 'UTF-8') . "</td>\r\n";
+		echo "<td>" . htmlspecialchars($class_info['start_date'], ENT_QUOTES, 'UTF-8') . "</td>\r\n";
+		echo "<td>" . htmlspecialchars($teacher_name, ENT_QUOTES, 'UTF-8') . "</td>\r\n";
+		echo "<td>" . htmlspecialchars($student_list, ENT_QUOTES, 'UTF-8') . "</td>\r\n";
+	}
+	echo "</tbody></table>";
+	/*
 	// Set local and session variables from the form data
-  $update = $_SESSION["update"] = $_POST['update']; // update is set to TRUE if this is updating an existing person or FALSE if this is adding a new person
-	$person_id = $_SESSION["person_id"] = $_POST['person_id'];
+  $person_id = $_SESSION["person_id"] = $_POST['person_id'];
 	$ptype_id = $_SESSION["ptype_id"] = $_POST['person_type'];
 	$family_name_k = $_SESSION["family_name_k"] = $_POST['family_name_k'];
 	$given_name_k = $_SESSION["given_name_k"] = $_POST['given_name_k'];
@@ -39,41 +61,19 @@ if(empty($_POST["confirm"])) {
 	$gender_id = $_SESSION["gender_id"] = $_POST['gender'];
 	$start_date = $_SESSION["start_date"] = $_POST['start_date'];
 	$end_date = $_SESSION["end_date"] = $_POST['end_date'] ?: "infinity"; // "infinity" means no end date
+*/
 
-	// get names instead of ID numbers for user confirmation
-	$ptype_name = get_ptype_by_id($ptype_id);
-	$gender_name = get_gender_by_id($gender_id);
-
-	echo "<h1>Please confirm the following information submitted</h1>";
-	echo "<table><tbody>";
-	echo "<tr><td><b>Person Type</b></td><td>". htmlspecialchars($ptype_name, ENT_QUOTES, 'UTF-8') ."</td></tr>";
-	echo "<tr><td><b>Family Name <span class=\"hint\">(in Kanji/Kana)</span></b></td><td>". htmlspecialchars($family_name_k, ENT_QUOTES, 'UTF-8') ."</td></tr>";
-	echo "<tr><td><b>Given Name <span class=\"hint\">(in Kanji/Kana)</span></b></td><td>". htmlspecialchars($given_name_k, ENT_QUOTES, 'UTF-8') ."</td></tr>";
-	echo "<tr><td><b>Family Name <span class=\"hint\">(in Romaji)</span></b></td><td>". htmlspecialchars($family_name_r, ENT_QUOTES, 'UTF-8') ."</td></tr>";
-	echo "<tr><td><b>Given Name <span class=\"hint\">(in Romaji)</span></b></td><td>". htmlspecialchars($given_name_r, ENT_QUOTES, 'UTF-8') ."</td></tr>";
-	if(isset($dob)) {
-		echo "<tr><td><b>Date of Birth</b></td><td>". htmlspecialchars(date("F j, Y",strtotime($dob))) ."</td></tr>";
-	}
-	else {
-		echo "<tr><td><b>Date of Birth</b></td><td>unknown</td></tr>";
-	}
-	echo "<tr><td><b>Gender</b></td><td>". htmlspecialchars($gender_name, ENT_QUOTES, 'UTF-8') ."</td></tr>";
-	echo "<tr><td><b>Start Date</b></td><td>". htmlspecialchars(date("F j, Y",strtotime($start_date)), ENT_QUOTES, 'UTF-8') ."</td></tr>";
-	if($end_date == "infinity") {
-		echo "<tr><td><b>End Date</b></td><td>none</td></tr>";
-	}
-	else {
-		echo "<tr><td><b>End Date</b></td><td>". htmlspecialchars(date("F j, Y",strtotime($end_date)), ENT_QUOTES, 'UTF-8') ."</td></tr>";
-	}
-	echo "</tbody></table>";
-
+	echo "<p class=\"afterform\"><strong>NOTE: </strong>When you confirm, all of the classes will be marked as ended on March 31, 2018.
+				Also, any students or teachers currently enrolled in these classes will be marked as ended in the roster.
+				This means you won't be able to access these past classes anymore with the online attendance system.
+				So please be sure only to do this after your attendance is current for these classes.</p>";
 	echo "<p class=\"afterform\">If the above information is correct, please choose \"confirm\".</p>";
 	echo "<form action=\"" . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . "\" method=\"post\">";
 	echo "<input type=\"hidden\" name=\"confirm\" value=\"TRUE\" / />";
 	echo "  <button type=\"submit\">Confirm</button>";
 	echo "</form>";
 }
-
+/*
 // The information has been confirmed
 else {
 	$update = $_SESSION["update"]; // update is set to TRUE if this is updating an existing person or FALSE if this is adding a new person
